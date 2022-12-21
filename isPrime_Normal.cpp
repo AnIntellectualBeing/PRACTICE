1f93efb86f439707d58ef28387e56eb371603e6d
E-25 <111394690+E-25@users.noreply.github.com>
2022-12-21 10:31:34 -0800

    Create isPrime_Normal.cpp

---
+++ isPrime_Normal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+bool isPrime(int n)
+{
+    for(int i=2;i<n/2;i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+   cout<< isPrime(7);
+
+    return 0;
+}
