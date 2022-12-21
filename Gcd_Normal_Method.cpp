7ca04bb14767afe4552af5fc4fc5c15bcb42719d
E-25 <111394690+E-25@users.noreply.github.com>
2022-12-21 08:48:19 -0800

    Create Gcd_Normal_Method.cpp

---
+++ Gcd_Normal_Method.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int gcd(int a,int b)
+{
+    int hcf=1;
+    int x= (a*(a<b)) + (b*(b<a));
+    cout<<x<<endl;
+    for(int i=1;i<=x;i++)
+    {
+        if(a%i==0 && b%i==0)
+            hcf=i;
+    }
+    return hcf;
+}
+int main()
+{
+    cout<<gcd(24,60);
+
+    return 0;
+}
